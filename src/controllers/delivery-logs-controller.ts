f01481b9ea9e8ba18e1e677bbb7cfbba85dfdf5e
Francisco <francisco.uberbh@outlook.com>
2026-01-12 11:28:02 -0300

    Cadastra Log

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,8 +1,33 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+import { z } from "zod"
+import { prisma } from "@/database/prisma"
 
 class DeliveryLogsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "ok" })
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    })
+
+    const { delivery_id, description } = bodySchema.parse(request.body)
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    })
+    if (!delivery) {
+      throw new AppError("Delivery not found", 404)
+    }
+    if (delivery.status === "processing") {
+      throw new AppError("change status to shipped")
+    }
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryID: delivery_id,
+        description,
+      },
+    })
+    return response.status(201).json()
   }
 }
 
